98043b993728de6c9b497dcc2209dc1576e6f487
Guillermo Gaxiola <saullogax@gmail.com>
2024-07-20 17:49:26 -0700

    reconocimiento de rostros

---
+++ ReconocimientoDerostros/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ReconocimientoDerostros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(654, 555);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(52, 573);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Cargar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(249, 573);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Video Live";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(414, 574);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 32);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Cargar Video";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnD.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnD.Location = new System.Drawing.Point(702, 358);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(113, 33);
+            this.btnD.TabIndex = 3;
+            this.btnD.Text = "<";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(851, 358);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 33);
+            this.button5.TabIndex = 3;
+            this.button5.Text = ">";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(681, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 340);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 613);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Map\'s Reconoce 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ReconocimientoDerostros/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Threading;
+
+namespace ReconocimientoDerostros
+{
+    public partial class Form1 : Form
+    {
+
+        //SE DECLARAN VARIABLES DE CAPTURA
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        List<Image> imgList = new List<Image>();
+        int i = 0;
+        int total = 0;
+
+        private CascadeClassifier _CascadeClassifier;
+        private CascadeClassifier _CascadeClassifier1;
+        private CascadeClassifier _CascadeClassifier2;
+        private CascadeClassifier _CascadeClassifier3;
+        public Form1()
+        {
+            InitializeComponent();
+            _CascadeClassifier = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+            _CascadeClassifier1 = new CascadeClassifier(Application.StartupPath + "/haarcascade_eye.xml");
+            _CascadeClassifier2 = new CascadeClassifier(Application.StartupPath + "/haarcascade_eye_tree_eyeglasses.xml");
+            _CascadeClassifier3 = new CascadeClassifier(Application.StartupPath + "/haarcascade_smile.xml");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Open = new OpenFileDialog();
+            if (Open.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> imagen = new Image<Bgr, byte>(Open.FileName);
+                //imageBox1.Image = imagen;
+
+                    using (var ImageFrame = imagen)
+                    {
+                        if (ImageFrame != null)
+                        {
+                            var grayFrame = ImageFrame.Convert<Gray, Byte>();
+                            //var grayFrame1 = ImageFrame.Convert<Gray, Byte>();
+                            //var grayFrame2 = ImageFrame.Convert<Gray, Byte>();
+                            //var grayFrame3 = ImageFrame.Convert<Gray, Byte>();
+
+
+                            var faces = _CascadeClassifier.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+                            var eyes = _CascadeClassifier1.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+                            var eyesglasses = _CascadeClassifier2.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+                            var smiles = _CascadeClassifier3.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+
+                            foreach (var face in faces)
+                            {
+                                //Detecta Rostros decimal la imagen
+                                ImageFrame.Draw(face, new Bgr(Color.Blue), 3);
+                                pictureBox1.Image = ImageFrame.GetSubRect(face).ToBitmap();
+                                imgList.Add(ImageFrame.GetSubRect(face).ToBitmap());
+                                total = total + 1;
+
+                            }
+
+                        foreach (var eye in eyes)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(eye, new Bgr(Color.Red), 3);
+                        }
+                        foreach (var eyesglasse in eyesglasses)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(eyesglasse, new Bgr(Color.Black), 3);
+                        }
+                        foreach (var smile in smiles)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(smile, new Bgr(Color.Brown), 3);
+                        }
+                    }
+                    imageBox1.Image = ImageFrame;
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture();
+            _captureThread = new Thread(DisplayWebCam);
+            _captureThread.Start();
+        }
+
+        private void DisplayWebCam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                //CvInvoke.Resize(frame, frame, imageBox1.Size);
+                imageBox1.Image = frame;
+
+                using (var ImageFrame = frame.ToImage<Bgr, Byte>())
+                {
+                    if (ImageFrame != null)
+                    {
+                        var grayFrame = ImageFrame.Convert<Gray, Byte>();
+                        var grayFrame1 = ImageFrame.Convert<Gray, Byte>();
+                        var grayFrame2 = ImageFrame.Convert<Gray, Byte>();
+                        var grayFrame3 = ImageFrame.Convert<Gray, Byte>();
+                        
+                        var faces = _CascadeClassifier.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+                        var eyes = _CascadeClassifier1.DetectMultiScale(grayFrame1, 1.1, 10, Size.Empty);
+                        var eyesglasses = _CascadeClassifier2.DetectMultiScale(grayFrame2, 1.1, 10, Size.Empty);
+                        var smiles = _CascadeClassifier3.DetectMultiScale(grayFrame3, 1.1, 10, Size.Empty);
+
+                        foreach (var face in faces)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(face, new Bgr(Color.Red), 3);
+                        }
+                        
+                        foreach (var eye in eyes)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(eye, new Bgr(Color.Red), 3);
+                        }
+                        foreach (var eyesglasse in eyesglasses)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(eyesglasse, new Bgr(Color.Black), 3);
+                        }
+                        foreach (var smile in smiles)
+                        {
+                            //Detecta Rostros decimal la imagen
+                            ImageFrame.Draw(smile, new Bgr(Color.Brown), 3);
+                        }
+                    }
+                    imageBox1.Image = ImageFrame;
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+            //SE CARGARA EL VIDEO
+            _capture = new VideoCapture(open.FileName);
+            _captureThread = new Thread(DisplayWebCam);
+            _captureThread.Start();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            --i;
+            if (i >= 0)
+            {
+                pictureBox1.Image = imgList[i];
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i++;
+            if (i <= 0)
+            {
+                pictureBox1.Image = imgList[i];
+            }
+        }
+
+    }
+}
